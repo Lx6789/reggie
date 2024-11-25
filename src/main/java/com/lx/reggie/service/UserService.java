bb2c067b833a9aff744549282f4648b03e7f4f38
Lu <dfhfkhhbsdagryt@163.com>
2024-11-25 23:25:27 +0800

    瑞吉外卖

--- src/main/java/com/lx/reggie/service/UserService.java
+++ src/main/java/com/lx/reggie/service/UserService.java
@@ -1,7 +1,14 @@
 package com.lx.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.lx.reggie.common.R;
 import com.lx.reggie.entity.User;
 
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
 public interface UserService extends IService<User> {
+    R<String> SendMsg(User user, HttpSession session);
+
+    R<User> login(Map map, HttpSession session);
 }
